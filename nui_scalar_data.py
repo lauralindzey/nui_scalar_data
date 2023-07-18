96db2f16744d403a28d8e747b535d03d04b161a8
Laura Lindzey <lindzey@uw.edu>
2023-07-18 13:53:13 +0200

    Pull field selection UI elements into separate QWidget

--- nui_scalar_data.py
+++ nui_scalar_data.py
@@ -103,6 +103,133 @@ def mdeglon(lat_deg):
     return dx
 
 
+class ScalarDataFieldWidget(QtWidgets.QWidget):
+    """
+    Widget that holds all the elements for adding a new field to the scalar
+    data widget.
+
+    emits:
+    * new_field(channel, msg_type_str, msg_field, sample_rate, layer_name)
+        Will be validated for existence of type+field
+    """
+
+    new_field = QtCore.pyqtSignal(str, str, str, float, str)
+
+    def __init__(self, iface, parent=None):
+        super(ScalarDataFieldWidget, self).__init__(parent)
+        self.iface = iface
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.grid = QtWidgets.QGridLayout()
+
+        row = 0
+        self.channel_name_label = QtWidgets.QLabel("Channel:")
+        self.channel_name_lineedit = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.channel_name_label, row, 0)
+        self.grid.addWidget(self.channel_name_lineedit, row, 1)
+        row += 1
+
+        self.msg_type_label = QtWidgets.QLabel("LCM type:")
+        self.msg_type_lineedit = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.msg_type_label, row, 0)
+        self.grid.addWidget(self.msg_type_lineedit, row, 1)
+        row += 1
+
+        self.msg_field_label = QtWidgets.QLabel("Field:")
+        self.msg_field_lineedit = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.msg_field_label, row, 0)
+        self.grid.addWidget(self.msg_field_lineedit, row, 1)
+        row += 1
+
+        self.sample_rate_label = QtWidgets.QLabel("Rate (Hz):")
+        self.sample_rate_lineedit = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.sample_rate_label, row, 0)
+        self.grid.addWidget(self.sample_rate_lineedit, row, 1)
+        row += 1
+
+        self.layer_name_label = QtWidgets.QLabel("Layer name:")
+        self.layer_name_lineedit = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.layer_name_label, row, 0)
+
+        self.grid.addWidget(self.layer_name_lineedit, row, 1)
+        row += 1
+
+        self.add_field_button = QtWidgets.QPushButton("Add Field")
+        self.add_field_button.clicked.connect(self.add_button_clicked)
+        self.grid.addWidget(self.add_field_button, row, 0, 1, 2)
+
+        self.setLayout(self.grid)
+
+    def add_button_clicked(self, _checked):
+        print("add_button_clicked")
+        channel_name = self.channel_name_lineedit.text()
+        if channel_name.strip() == "":
+            errmsg = "Please select non-empty channel name."
+            print(errmsg)
+            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+            QgsMessageLog.logMessage(errmsg)
+            return
+        print(f"channel_name: {channel_name}")
+
+        msg_type_str = self.msg_type_lineedit.text()
+        msg_type = None
+        msg = None
+        try:
+            msg_pkg, msg_class = msg_type_str.split(".")
+            msg_module = importlib.import_module(msg_pkg)
+            msg_type = getattr(msg_module, msg_class)
+            msg = msg_type()
+            if not hasattr(msg, "utime"):
+                errmsg = "Plotted messages must have utime field!"
+                print(errmsg)
+                self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+                QgsMessageLog.logMessage(errmsg)
+                return
+        except Exception as ex:
+            errmsg = f"Tried to instantiate a '{msg_type_str}'. Got exception {ex}"
+            print(errmsg)
+            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+            QgsMessageLog.logMessage(errmsg)
+            return
+        print(f"msg_type = {msg_type_str}")
+
+        # QUESTION: Do we need to support nested fields?
+        msg_field = self.msg_field_lineedit.text()
+        if not hasattr(msg, msg_field):
+            errmsg = (
+                f"Message of type '{msg_type_str}' does not have field '{msg_field}'"
+            )
+            print(errmsg)
+            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+            QgsMessageLog.logMessage(errmsg)
+            return
+        print(f"msg_field = {msg_field}")
+
+        sample_rate_str = self.sample_rate_lineedit.text()
+        try:
+            sample_rate = float(sample_rate_str)
+        except Exception as ex:
+            errmsg = "Couldn't convert input '{sample_rate_str}' into float."
+            print(errmsg)
+            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+            QgsMessageLog.logMessage(errmsg)
+            return
+
+        layer_name = self.layer_name_lineedit.text()
+        if layer_name.strip() == "":
+            errmsg = "Please select non-empty layer name."
+            print(errmsg)
+            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+            QgsMessageLog.logMessage(errmsg)
+            return
+        print(f"layer_name: {layer_name}")
+
+        self.new_field.emit(
+            channel_name, msg_type_str, msg_field, sample_rate, layer_name
+        )
+
+
 class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
     # If I understand correctly, any slots decorated with @pyqtSlot will be
     # called in the thread that created the connection, NOT the thread that
@@ -117,17 +244,18 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
         super(NuiScalarDataMainWindow, self).__init__(parent)
         self.iface = iface
 
+        self.config = []  # This gets updated by the add_field method
         try:
             config_str, success = QgsProject.instance().readEntry(
                 "nui_scalar_data", "subscriptions"
             )
             if success:
-                self.config = yaml.safe_load(config_str)
-                print(f"Loaded config! {self.config}")
+                self.loaded_config = yaml.safe_load(config_str)
+                print(f"Loaded config! {self.loaded_config}")
             else:
-                self.config = []
+                self.loaded_config = []
         except Exception as ex:
-            self.config = []
+            self.loaded_config = []
 
         self.setup_ui()
 
@@ -184,48 +312,12 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
         self.shutdown = False
 
     def setup_ui(self):
-        self.subscription_grid = QtWidgets.QGridLayout()
-
-        row = 0
-        self.channel_name_label = QtWidgets.QLabel("Channel:")
-        self.channel_name_lineedit = QtWidgets.QLineEdit()
-        self.subscription_grid.addWidget(self.channel_name_label, row, 0)
-        self.subscription_grid.addWidget(self.channel_name_lineedit, row, 1)
-        row += 1
-
-        self.msg_type_label = QtWidgets.QLabel("LCM type:")
-        self.msg_type_lineedit = QtWidgets.QLineEdit()
-        self.subscription_grid.addWidget(self.msg_type_label, row, 0)
-        self.subscription_grid.addWidget(self.msg_type_lineedit, row, 1)
-        row += 1
-
-        self.msg_field_label = QtWidgets.QLabel("Field:")
-        self.msg_field_lineedit = QtWidgets.QLineEdit()
-        self.subscription_grid.addWidget(self.msg_field_label, row, 0)
-        self.subscription_grid.addWidget(self.msg_field_lineedit, row, 1)
-        row += 1
-
-        self.sample_rate_label = QtWidgets.QLabel("Rate (Hz):")
-        self.sample_rate_lineedit = QtWidgets.QLineEdit()
-        self.subscription_grid.addWidget(self.sample_rate_label, row, 0)
-        self.subscription_grid.addWidget(self.sample_rate_lineedit, row, 1)
-        row += 1
-
-        self.layer_name_label = QtWidgets.QLabel("Layer name:")
-        self.layer_name_lineedit = QtWidgets.QLineEdit()
-        self.subscription_grid.addWidget(self.layer_name_label, row, 0)
-        self.subscription_grid.addWidget(self.layer_name_lineedit, row, 1)
-        row += 1
-
-        self.add_field_button = QtWidgets.QPushButton("Add Field")
-        self.add_field_button.clicked.connect(self.add_button_clicked)
-        self.subscription_grid.addWidget(self.add_field_button, row, 0, 1, 2)
+        self.data_selector = ScalarDataFieldWidget(self.iface)
+        self.data_selector.new_field.connect(self.add_field)
 
         self.fig = Figure((8.0, 4.0), dpi=100)
         self.ax = self.fig.add_axes([0.1, 0.15, 0.8, 0.8])
         self.cursor_vline = self.ax.axvline(0, 0, 1, ls="--", color="grey")
-        print("Functions defined for vline: ", dir(self.cursor_vline))
-        # TODO: Turn off this one's y-labels/ticks?, because we won't use it, other than for twinning?
         self.time_formatter = FuncFormatter(
             lambda tt, pos: datetime.datetime.utcfromtimestamp(tt).strftime("%H:%M:%S")
         )
@@ -253,7 +345,7 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
         #   chunks of data on the plot
 
         self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.addLayout(self.subscription_grid)
+        self.vbox.addWidget(self.data_selector)
         self.vbox.addWidget(QHLine())
         self.vbox.addStretch(1.0)
 
@@ -312,94 +404,6 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
         # TODO: Add layer on map with cursor
         # TODO: Add cursor
         # TODO: Add second axis, with twinx?
-
-    def add_button_clicked(self, _checked):
-        print("add_button_clicked")
-
-        if not self.projection_initialized:
-            errmsg = "Waiting on DIVE_INI; cannot add layers."
-            print(errmsg)
-            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-            QgsMessageLog.logMessage(errmsg)
-            return
-
-        channel_name = self.channel_name_lineedit.text()
-        if channel_name.strip() == "":
-            errmsg = "Please select non-empty channel name."
-            print(errmsg)
-            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-            QgsMessageLog.logMessage(errmsg)
-            return
-        print(f"channel_name: {channel_name}")
-
-        msg_type_str = self.msg_type_lineedit.text()
-        msg_type = None
-        msg = None
-        try:
-            msg_pkg, msg_class = msg_type_str.split(".")
-            self.msg_modules[msg_pkg] = importlib.import_module(msg_pkg)
-            msg_type = getattr(self.msg_modules[msg_pkg], msg_class)
-            msg = msg_type()
-            if not hasattr(msg, "utime"):
-                errmsg = "Plotted messages must have utime field!"
-                print(errmsg)
-                self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-                QgsMessageLog.logMessage(errmsg)
-                return
-        except Exception as ex:
-            errmsg = f"Tried to instantiate a '{msg_type_str}'. Got exception {ex}"
-            print(errmsg)
-            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-            QgsMessageLog.logMessage(errmsg)
-            return
-        print(f"msg_type = {msg_type_str}")
-
-        # QUESTION: Do we need to support nested fields?
-        msg_field = self.msg_field_lineedit.text()
-        if not hasattr(msg, msg_field):
-            errmsg = (
-                f"Message of type '{msg_type_str}' does not have field '{msg_field}'"
-            )
-            print(errmsg)
-            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-            QgsMessageLog.logMessage(errmsg)
-            return
-        print(f"msg_field = {msg_field}")
-
-        sample_rate_str = self.sample_rate_lineedit.text()
-        try:
-            sample_rate = float(sample_rate_str)
-        except Exception as ex:
-            errmsg = "Couldn't convert input '{sample_rate_str}' into float."
-            print(errmsg)
-            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-            QgsMessageLog.logMessage(errmsg)
-            return
-
-        layer_name = self.layer_name_lineedit.text()
-        if layer_name.strip() == "":
-            errmsg = "Please select non-empty layer name."
-            print(errmsg)
-            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
-            QgsMessageLog.logMessage(errmsg)
-            return
-        print(f"layer_name: {layer_name}")
-
-        # TODO: Call function adding layer. Will need to check whether our projection has been initialized.
-        # Ah! This probably can't be a function call, unless we move everything
-        # into the widget.
-        self.config.append(
-            [channel_name, msg_type_str, msg_field, sample_rate, layer_name]
-        )
-        # NOTE(lindzey): It might be more idiomatic to save this on close, rather than
-        #  at every update?
-        print(f"Trying to save config. type = {type(self.config)}: {self.config}")
-        config_str = yaml.safe_dump(self.config)
-        print(f"Resulting yaml string: {config_str}")
-        QgsProject.instance().writeEntry(
-            "nui_scalar_data", "subscriptions", yaml.safe_dump(self.config)
-        )
-        self.add_field(channel_name, msg_type_str, msg_field, sample_rate, layer_name)
 
     def handle_statexy(self, channel, data):
         """ "
@@ -553,7 +557,13 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
         self.update_subscriptions()  # Activate any subscriptions from the config
 
     def update_subscriptions(self):
-        for channel, msg_type_str, msg_field, sample_rate, layer_name in self.config:
+        for (
+            channel,
+            msg_type_str,
+            msg_field,
+            sample_rate,
+            layer_name,
+        ) in self.loaded_config:
             self.add_field(channel, msg_type_str, msg_field, sample_rate, layer_name)
 
     def setup_layers(self):
@@ -607,6 +617,20 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
         """
         Subscribe to specified data and plot in both map and profile view.
         """
+        if not self.projection_initialized:
+            errmsg = "Waiting on DIVE_INI; cannot add layers."
+            print(errmsg)
+            self.iface.messageBar().pushMessage(errmsg, level=Qgis.Warning)
+            QgsMessageLog.logMessage(errmsg)
+            return
+
+        self.config.append([channel, msg_type_str, msg_field, sample_rate, layer_name])
+        # NOTE(lindzey): It might be more idiomatic to save this on close, rather than
+        #  at every update?
+        QgsProject.instance().writeEntry(
+            "nui_scalar_data", "subscriptions", yaml.safe_dump(self.config)
+        )
+
         key = f"{channel}/{msg_field}"
         print(f"add_field for key={key}")
         if key in self.layers:
@@ -696,8 +720,6 @@ class NuiScalarDataMainWindow(QtWidgets.QMainWindow):
                 msg_type, msg_field, channel, data
             ),
         )
-
-        # Create the axis to plot onto
 
     def handle_data(self, msg_type, msg_field, channel, data):
         key = f"{channel}/{msg_field}"
